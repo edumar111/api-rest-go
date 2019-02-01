b0db1f56f2097444b980d36a35dd51d123e6ae94
Eduardo Marchena <emarchen@everis.com>
2019-02-01 11:40:32 -0500

    router gorilla mux

--- main.go
+++ main.go
@@ -5,14 +5,23 @@ import (
 	"log"
 
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from server web Go")
-	})
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/contac", Contact)
 	fmt.Println("server listen port 8080")
-	server := http.ListenAndServe(":8080", nil)
+	server := http.ListenAndServe(":8080", router)
 	log.Fatal(server)
 
+}
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from server web Go")
+}
+func Contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "page contac")
 }
