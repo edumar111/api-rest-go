b6f401f5580f00c74fad7fdbba28505830357caa
Eduardo Marchena <emarchen@everis.com>
2019-02-01 14:59:08 -0500

    add and list of mongo

--- actions.go
+++ actions.go
@@ -7,21 +7,51 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	mgo "gopkg.in/mgo.v2"
 )
 
-var movies = Movies{
-	Movie{"Sin Limites", 2013, "Desconocido"},
-	Movie{"Batman Begin", 2013, "Leo saple"},
-	Movie{"Top Gouns", 2013, "George Lucas"},
+//====MONGO=========================
+func getSession() *mgo.Session {
+	session, err := mgo.Dial("mongodb://localhost")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return session
 }
 
+var collection = getSession().DB("netflix").C("movies")
+
+//===HEAD METHODS======================
+func responseMovie(w http.ResponseWriter, status int, results Movie) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(results)
+}
+
+func responseMovies(w http.ResponseWriter, status int, results []Movie) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(results)
+}
+
+//==METHOD ROUTES======================
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello from server web Go")
 }
 func MovieList(w http.ResponseWriter, r *http.Request) {
 	log.Println("MovieList")
-	//fmt.Fprintf(w, "List Movies")
-	json.NewEncoder(w).Encode(movies)
+	var results []Movie
+	err := collection.Find(nil).Sort("-_id").All(&results)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Resultados: ", results)
+	}
+
+	responseMovies(w, 200, results)
 }
 func MovieShow(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
@@ -37,7 +67,13 @@ func MovieAdd(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 	defer r.Body.Close()
-	log.Println(movie_data)
-	movies = append(movies, movie_data)
+	//log.Println(movie_data)
+	err = collection.Insert(movie_data)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	responseMovie(w, 200, movie_data)
 
 }
